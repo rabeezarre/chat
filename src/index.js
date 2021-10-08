124158ff433cee8160acc15b7d23575ed202fbbc
rabeezarre <rabina.abdrakhmanova@nu.edu.kz>
2021-10-08 13:52:54 +0600

    first commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+//import dotenv from 'dotenv';
+//dotenv.config()
+// require('dotenv').config()
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
